3a0fcef54305d29efeff74229634d56abfba2841
Patrick Weed <elisha.productions@gmail.com>
2018-11-07 16:50:37 -0800

    cleanup

--- get_token.py
+++ get_token.py
@@ -14,13 +14,8 @@ CLIENT_ID = 'afac0221155d4a6b8ef3e3e2f4909635'
 CLIENT_SECRET = 'ccf370c58bbe4d22aaf0cb7ac39193a2'
 
 token = util.prompt_for_user_token(username,scope,client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri='https://localhost:4567/callback')
-
-# if token:
-#     sp = spotipy.Spotify(auth=token)
-#     sp.trace = False
-#     playlists = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
-#     pprint.pprint(playlists)
-# else:
-#     print("Can't get token for", username)
-
-print('token')
+my_file = os.path.isfile('.cache-drummerweed')
+if my_file is True:
+    print('Token has been cached, proceed to using Maestro')
+else:
+    print('Token could not be saved')
